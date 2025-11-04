9cad49717a19b64734a9f6e637fad71f204f0d3e
JiuShuang <1017299634@qq.com>
2025-11-04 16:11:07 +0800

    first commit

---
+++ SwiftUILearningJourney/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUILearningJourney
+//
+//  Created by Mac on 2025/11/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUILearningJourney/SwiftUILearningJourneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILearningJourneyApp.swift
+//  SwiftUILearningJourney
+//
+//  Created by Mac on 2025/11/4.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILearningJourneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
